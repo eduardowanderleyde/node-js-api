ed5e10d6205df708d2e2134ab46aaa842135ad20
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-04-26 15:56:23 -0300

    Method Update

--- index.js
+++ index.js
@@ -21,4 +21,24 @@ server.get('/customers/:id', (req, res) => {
     return res.status(status).json(customer);
 });
 
+server.post("/customers", (req, res) => {
+    const { name, site} = req.body;
+    const id = customers[customers.length - 1].id + 1;
+    const newCustomer = {id, name, site };
+    customers.push(newCustomer);
+
+    return res.status(201).json(newCustomer);
+});
+
+server.put( "/customers/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, site } = req.body;
+    const index = customers.findIndex(item => item.id === id);
+    const status = index >= 0 ? 200 : 404;
+
+    if(index >= 0){
+        customers[index] = { id, name, site };
+    }
+    return res.status(status).json(customers[index]);
+});
 server.listen(3000);
